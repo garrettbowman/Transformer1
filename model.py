0eb22b11468e6f4f258c5f87b753b12e98d03c1e
Garrett <garrettbowman96@gmail.com>
2024-01-28 05:44:00 -0500

    Add MultiHeadAttentionBlock and ResidualConnectionBlock classes

--- model.py
+++ model.py
@@ -50,3 +50,43 @@ class FeedForwardBlock(nn.Module):
 
     def forward(self, x):
         return self.linear2(self.dropout(F.relu(self.linear1(x))))
+    
+class MultiHeadAttentionBlock(nn.Module):
+    def __init__(self, embedding_size: int, num_heads: int, dropout: float):
+        super(MultiHeadAttentionBlock, self).__init__()
+        self.embedding_size = embedding_size
+        self.num_heads = num_heads
+        assert embedding_size % num_heads == 0,"embedding size not be divisible by num_heads"
+        
+        self.head_size = embedding_size // num_heads
+        self.scale = math.sqrt(self.head_size)
+        self.dropout = nn.Dropout(dropout)
+        self.query = nn.Linear(embedding_size, embedding_size)
+        self.key = nn.Linear(embedding_size, embedding_size)
+        self.value = nn.Linear(embedding_size, embedding_size)
+        self.output = nn.Linear(embedding_size, embedding_size)
+
+    def forward(self, x):
+        batch_size = x.shape[0]
+        query = self.query(x).view(batch_size, -1, self.num_heads, self.head_size).transpose(1, 2)
+        key = self.key(x).view(batch_size, -1, self.num_heads, self.head_size).transpose(1, 2)
+        value = self.value(x).view(batch_size, -1, self.num_heads, self.head_size).transpose(1, 2)
+        attention = self.attention(query, key, value)
+        attention = attention.transpose(1, 2).contiguous().view(batch_size, -1, self.embedding_size)
+        return self.output(attention)
+
+    @staticmethod
+    def attention(self, query, key, value):
+        scores = torch.matmul(query, key.transpose(-2, -1)) / self.scale
+        p_attn = F.softmax(scores, dim=-1)
+        p_attn = self.dropout(p_attn)
+        return torch.matmul(p_attn, value)
+    
+class ResidualConnectionBlock(nn.Module):
+    def __init__(self, embedding_size: int, dropout: float):
+        super(ResidualConnectionBlock, self).__init__()
+        self.layer_norm = LayerNormalization(embedding_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.layer_norm(x)))
