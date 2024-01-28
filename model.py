dc8af9e81ad9fc49d11e02279d906372a984a33d
Garrett <garrettbowman96@gmail.com>
2024-01-28 04:10:11 -0500

    Add LayerNormalization class to model.py

--- model.py
+++ model.py
@@ -28,3 +28,15 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + self.pe[:, :x.shape(1), :].requires_grad_(False)
         return self.dropout(x)
+    
+class LayerNormalization(nn.Module):
+    def __init__(self, embedding_size: int, eps: float = 1e-6):
+        super(LayerNormalization, self).__init__()
+        self.alpha = nn.Parameter(torch.ones(embedding_size)) #multiplied
+        self.beta = nn.Parameter(torch.zeros(embedding_size)) #added
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.beta
